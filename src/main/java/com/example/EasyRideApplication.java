a223e8e2e8298b1ddaba03088d1c050271d765f7
mohamed <elmelianimohamed6@gmail.com>
2022-05-04 23:46:20 +0000

    new version v2

--- src/main/java/com/example/EasyRideApplication.java
+++ src/main/java/com/example/EasyRideApplication.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.LocalDate;
+
 
 @SpringBootApplication
 public class EasyRideApplication  implements WebMvcConfigurer{
