e9ce4d41b746986d4161e4c56a3ed6fc4e70fac7
mohamed <elmelianimohamed6@gmail.com>
2022-05-05 00:46:52 +0000

    new version v2

--- src/main/java/com/example/utilitis/JWTUtilities.java
+++ src/main/java/com/example/utilitis/JWTUtilities.java
@@ -4,6 +4,6 @@ public class JWTUtilities {
     public static final String SECRET="Mysecret1234";
     public static final String PREFIX="Bearer ";
     public static final String AUTH_HEADER="Authorization";
-    public static final long EXPIRE_ACCESS_TOKEN=2*60*1000;
+    public static final long EXPIRE_ACCESS_TOKEN=5*60*1000;
     public static final long EXPIRE_REFRESH_TOKEN=24*60*60*1000;
 }
